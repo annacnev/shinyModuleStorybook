c21b728d5b4d7c2e03157850812428674ccbb973
Kyle Barrett <barrettk@metrumrg.com>
2021-03-31 14:31:45 -0400

    feature: download module(s) as zip

--- app/app.R
+++ app/app.R
@@ -17,7 +17,14 @@ shinyApp(
                                                                menuSubItem("Event Table 1",tabName = "eventTable1"),
                                                                menuSubItem("Event Table 2",tabName = "eventTable2")
                                                       )
-                                                    )
+                                                    ),
+                                                    hr(),
+                                                    fluidRow(
+                                                      column(8,offset=2,style="text-align:center",
+                                                             actionBttn("exportResultsModal", "Export Module(s)", 
+                                                                        icon = icon("download"), size = 'sm',color="success"))
+                                                    ),
+                                                    hr()
                          ),
                          body = dashboardBody(
                            tags$head(
@@ -28,7 +35,7 @@ shinyApp(
                              tabItem("dashboard", uiOutput("dashboard")),
                              tabItem("mod-example", htmlOutput("module_frame")),
                              tabItem("eventTable1", eventTable_UI("event_table1")),
-                             tabItem("eventTable2", event_table_ui('event_table2'))
+                             tabItem("eventTable2", event_table_ui("event_table2"))
                            )
                          ),
                          title = "shinyDashboardPlus",
@@ -76,7 +83,47 @@ shinyApp(
     
     eventTable2 <- event_table_server('event_table2')
     
-
+    
+    # Download Module ---------------------------------------------------------
+    
+    observeEvent(input$exportResultsModal, {
+      showModal(export_results_modal())
+    })
+    
+    output$exportModules <- downloadHandler(
+      filename = function() {
+        paste("Modules-", Sys.Date(), ".zip", sep="")
+      },
+      content = function(file) {
+        temp_dir <- tempdir()
+        message(temp_dir)
+        data_to_export <- lapply(1:length(input$modulesToExport),function(i){
+          files <- list.files(file.path("modules",input$modulesToExport[i]),recursive = T)
+          sub_folders <- dirname(files[grep("/",files)])
+          fs::dir_create(file.path(temp_dir,"modules",input$modulesToExport[i]), recurse = TRUE)
+          fs::dir_create(file.path(temp_dir,"modules",input$modulesToExport[i],sub_folders), recurse = TRUE)
+          file.path(input$modulesToExport[i],files)
+        }) %>% unlist()
+        filesToZip <- c()
+        for(file.i in data_to_export){
+          this_file.i <- file.path(temp_dir,"modules" , file.i)
+          if(!fs::file_exists(this_file.i)){
+            fs::file_copy(file.path("modules",file.i), this_file.i)
+          }
+          filesToZip <- c(filesToZip, this_file.i)
+          rm(this_file.i)
+        }
+        zip(zipfile = file, files = filesToZip, flags = "-r9Xj")
+        
+        for(file.i in filesToZip){
+          file.remove(file.i)
+        }
+        removeModal()
+      }
+    )
+    observeEvent(input$cancelExport, {
+      removeModal()
+    })
     
   } # End Server
 )
